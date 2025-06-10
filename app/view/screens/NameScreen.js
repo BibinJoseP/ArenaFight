a8e336121799669fe252de2f85da029f997f640e
BibinJoseP <bibinjosepaul@.com>
2025-06-10 15:49:39 +0530

    name screen updated

--- app/view/screens/NameScreen.js
+++ app/view/screens/NameScreen.js
@@ -1,28 +1,55 @@
-Ext.define('ArenaFight.view.screens.NameScreen', {
-    extend: 'Ext.Container',
-    xtype: 'namescreen',
+Ext.define("ArenaFight.view.screens.NameScreen", {
+    extend: "Ext.Container",
+    xtype: "namescreen",
 
-    config: {
-        layout: 'vbox',
-        style: 'background:#222; color:white;',
-        items: [
-            {
-                xtype: 'textfield',
-                label: 'Enter Fighter Name',
-                margin: 20,
-                id: 'fighterNameInput'
-            },
-            {
-                xtype: 'button',
-                text: 'Continue to Fight',
-                ui: 'confirm',
-                margin: 20,
-                handler: function () {
-                    const name = Ext.getCmp('fighterNameInput').getValue();
-                    localStorage.setItem('fighterName', name);
-                    Ext.ComponentQuery.query('app-main')[0].setActiveItem(2); // Go to FightScreen
-                }
-            }
-        ]
-    }
+    requires: [
+        "Ext.form.Panel", 
+        "Ext.field.Text", 
+        "Ext.Button",
+        "Ext.MessageBox",
+        "ArenaFight.view.screens.FightScreen" // Ensure FightScreen is required
+    ],
+
+    layout: "fit",
+    cls: 'name-screen-container', // For CSS styling
+
+    items: [{
+        xtype: "formpanel",
+        scrollable: false,
+        cls: 'arena-namescreen-form-panel',
+        layout: {
+            type: "vbox",
+            align: "middle",
+            pack: "center"
+        },
+        items: [{
+            xtype: "component",
+            cls: 'name-screen-title',
+            html: "Enter Your Fighter Name"
+        }, {
+            xtype: "textfield",
+            name: "fighterName",
+            placeHolder: "Fighter Name",
+            clearable: false,
+            required: true,
+            cls: 'x-namescreen-fighter-name-field'
+        }, {
+            xtype: "button",
+            text: "START BATTLE",
+            margin: "25 0 0 0",
+            cls: 'name-screen-button',
+                 handler: function (btn) {
+              const form = btn.up("formpanel");
+              const values = form.getValues();
+
+              if (values.fighterName && values.fighterName.trim().length > 0) {
+                localStorage.setItem("fighterName", values.fighterName.trim());
+                Ext.Viewport.setActiveItem({ xtype: "fightscreen" });
+              } else {
+                Ext.Msg.alert("Oops!", "Please enter a name to proceed.");
+              }}
+        }]
+    }],
+
+
 });
